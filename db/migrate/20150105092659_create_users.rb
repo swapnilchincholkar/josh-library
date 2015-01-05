99aa920a58e62034411c7453e6e8b34177dacdb2
Pratik Shah <pratik@joshsoftware.com>
2015-01-05 15:14:32 +0530

    Device settings

--- db/migrate/20150105092659_create_users.rb
+++ db/migrate/20150105092659_create_users.rb
@@ -2,12 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string  :email
-      t.string  :password_digest
       t.string  :first_name
       t.string  :last_name
-
       t.timestamps
-
       t.index [:email]
     end
   end
